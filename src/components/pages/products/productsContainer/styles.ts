06cbef6fa5ec8b90b2d253afdcf656226ac54e32
nejirah <nejira.hasanbegovic@edu.fit.ba>
2023-04-19 04:00:19 -0700

    fix: changed style of products container

--- src/components/pages/products/productsContainer/styles.ts
+++ src/components/pages/products/productsContainer/styles.ts
@@ -1,6 +1,10 @@
 import { Grid, styled } from '@mui/material';
 
 export const GridStyled = styled(Grid)`
-    display: 'flex',
-    alignItems: 'center'
+    display: flex;
+    align-items: center;
+`;
+
+export const GridEndStyled = styled(GridStyled)`
+    justify-content: flex-end;
 `;
