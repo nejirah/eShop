946ae70e67e10ae6299c2a6bada07eea6706b296
nejirah <nejira.hasanbegovic@edu.fit.ba>
2023-04-27 01:32:12 -0700

    fix: code formatted

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,8 +2,8 @@ import { get } from './GetProducts';
 import { getById } from './GetProductById';
 
 export const GetProducts = () => {
-    return get();
+  return get();
 };
 export const GetProductsById = (id: number) => {
-    return getById(id);
-}
+  return getById(id);
+};
